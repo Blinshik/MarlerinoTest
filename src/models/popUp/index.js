1b69808e2ca5b834efbf761cd594f057a4c627ae
Alexey <lyahov_alexey@mail.ru>
2021-06-10 15:32:39 +0500

    fix attache btn

--- src/models/popUp/index.js
+++ src/models/popUp/index.js
@@ -1,16 +1,14 @@
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 import styles from './popUp.module.scss'
 import { setImg } from '../../redux/messages'
 import { useDispatch } from 'react-redux'
 
 export default function PopUp({open}) {
-    const [openLoader, setOpenLoader] = useState(false)
     const inputRef = useRef(null)
     const dispatch = useDispatch()
 
     const closeHandler = () => {
         open(false)
-        setOpenLoader(false)
     }
 
     const handleSubmit = (e) => {
@@ -20,10 +18,6 @@ export default function PopUp({open}) {
         }
     }
 
-    const openHandler = () => {
-        setOpenLoader(true)
-    }
-
     return (
         <div className={styles.container}>
             <div className={styles.container__header}>
@@ -31,13 +25,25 @@ export default function PopUp({open}) {
                 <button 
                     className={styles.container__header__close}
                     onClick={closeHandler}
-                >X</button>
+                >
+                    <span className={"material-icons"}>
+                        close
+                    </span>
+                </button>
             </div>
-            <button className={styles.container__addContent} onClick={openHandler}>
-                    <div className={styles.container__addContent__text}>Фото или видео</div>    
-            </button>
-            <form style={{ display: openLoader ? 'block' : 'none' }} ref={inputRef} >
-                <input type="file" onChange={handleSubmit} />
+            
+            <form ref={inputRef} style={{ width: "100%" }}  >
+                <input 
+                    type="file" 
+                    onChange={handleSubmit} 
+                    id={"file"} 
+                    style={{ display: 'none' }} 
+                />
+                <label for="file">
+                    <div className={styles.container__addContent}>
+                        <div className={styles.container__addContent__text}>Фото или видео</div>  
+                    </div>  
+                </label>
             </form>
             <button className={styles.container__addContent}>
                 <div className={styles.container__addContent__text}>Заявку</div>  
